f4157e3d87188cddc2b0fbeef17b13a967a378df
ishakir <imran.pshakir@gmail.com>
2019-07-17 11:46:43 +0800

    Add some timing to round logging so that we know a bit better what's going on

--- tictactoe799/__main__.py
+++ tictactoe799/__main__.py
@@ -4,9 +4,18 @@ from tictactoe799.player.machine_learning_player import MachineLearningPlayer, l
 from tictactoe799.player.minimax_player import MinimaxPlayer
 
 from collections import defaultdict
+from contextlib import contextmanager
 import click
-
 from os import makedirs, path
+import sys
+from time import time
+
+@contextmanager
+def timing(log_statement):
+	before = time()
+	yield
+	after = time()
+	print("{} took {:.0f} seconds".format(log_statement, after - before))
 
 @click.command()
 @click.option('--resume-from-round', default=0)
@@ -20,7 +29,7 @@ def run(resume_from_round, data_root):
 	minimax_play_first = MinimaxPlayer(True)
 	minimax_play_second = MinimaxPlayer(False)
 
-	for i in range(resume_from_round, 10000):
+	for i in range(resume_from_round, 100000):
 		print('ROUND {}'.format(str(i)))
 
 		round_dir = path.join(data_root, 'output', str(i))
@@ -34,29 +43,31 @@ def run(resume_from_round, data_root):
 		makedirs(minimax_result_dir, exist_ok=True)
 		makedirs(training_dir, exist_ok=True)
 
-		print('- Saving players')
-		for player in players:
-			player.save(players_dir)
+		with timing('- Saving players'):
+			for player in players:
+				player.save(players_dir)
 		
-		print('- Playing Matches')
-		round = Round(round_robin_mix(players))
-		round.play_all()
+		with timing('- Playing Matches'):
+			round = Round(round_robin_mix(players))
+			round.play_all()
 
-		print('- Saving Round')
-		round.save(round_robin_results_dir)
+		with timing('- Saving Round'):
+			round.save(round_robin_results_dir)
 
-		print('- Playing and saving results against minimax player')
-		for player in players:
-			player.training_mode = False
-		minimax_round = Round(all_v_one_mix(players, minimax_play_first, True) + all_v_one_mix(players, minimax_play_second, False))
-		minimax_round.play_all()
-		minimax_round.save(minimax_result_dir)
-		for player in players:
-			player.training_mode = True
+		with timing('- Comparing against benchmark'):
+			for player in players:
+				player.training_mode = False
+			minimax_round = Round(all_v_one_mix(players, minimax_play_first, True) + all_v_one_mix(players, minimax_play_second, False))
+			minimax_round.play_all()
+			minimax_round.save(minimax_result_dir)
+			for player in players:
+				player.training_mode = True
 
-		print('- Training'.format(i))
-		for x in players:
-			x.new_batch(training_dir)
+		with timing('- Training'.format(i)):
+			for x in players:
+				x.new_batch(training_dir)
+
+		sys.stdout.flush()
 
 
 if __name__ == '__main__':
